d30d2d356ea8691deadef881ff2b7ad5e4c0b834
Nick Busey <nick@Nicks-iMac.local>
2016-04-02 13:35:08 -0600

    Adding examples

--- README.md
+++ README.md
@@ -8,11 +8,13 @@ Install Tampermonkey for Chrome. Go to workflowy.com, click the Tampermonkey ico
 ## Current features
 
  - Adds 1wk and 24hrs buttons to the header to show recently changed items.
+ - Random hashtag functionality. Any hashtags with -rand at the end will go to another random item with the same hashtag. Basically recreates the functionality of flash cards.
+
+  Example: #spanish-rand for spanish words you want to study. #capitals-rand for capitals.
 
 ## Planned features
 
  - Show percentage of items with a certain hashtag that are 'complete'.
  - A view of all tags on side of window, tag cloud. Ordered by frequency.
  - Show streak of recently created items with hashtags that have been 'completed'. This will replicate "don't break the chain" functionality.
- - Random hashtag functionality. Any hashtags with -rand at the end will go to another random item with the same hashtag. Basically creates 'flash cards'.
  - Better install?
--- script.js
+++ script.js
@@ -1,9 +1,11 @@
+
 // ==UserScript==
-// @name         jQuery For Chrome (A Cross Browser Example)
-// @namespace    jQueryForChromeExample
+// @name         Workflowier
+// @namespace    Workflowier
 // @include      *
-// @author       Erik Vergobbi Vold & Tyler G. Hicks-Wright
-// @description  This userscript is meant to be an example on how to use jQuery in a userscript on Google Chrome.
+// @author       Nick Busey
+// @description  User Script for Workflowy.com that adds some extra features.
+// @match        https://workflowy.com/#
 // ==/UserScript==
 
 // a function that loads jQuery and calls a callback function when jQuery has finished loading
@@ -18,28 +20,59 @@ function addJQuery(callback) {
   document.body.appendChild(script);
 }
 
+searching = false;
+
 // the guts of this userscript
 function main() {
   // Note, jQ replaces jQ to avoid conflicts.
   // Insert recent links
-  jQ('#savedViewHUDButton').after("<div class='showCompletedButton button'><div class='topBarButtonTextContainer'><a href='#' id='recentLink_1wk'>1wk</a></div></div>");
-  jQ('#recentLink_1wk').click(function(e) {
-    e.preventDefault();
-    if (jQ('#searchBox').val()=='last-changed:7d') {
-      search.searchProjectTree('');
-    } else {
-      search.searchProjectTree('last-changed:7d');
-    }
-  });
-  jQ('#savedViewHUDButton').after("<div class='showCompletedButton button'><div class='topBarButtonTextContainer'><a href='#' class='button' id='recentLink_24hrs'>24hrs</a></div></div>");
-  jQ('#recentLink_24hrs').click(function(e) {
-    e.preventDefault();
-    if (jQ('#searchBox').val()=='last-changed:1d') {
-      search.searchProjectTree('');
-    } else {
-      search.searchProjectTree('last-changed:1d');
-    }
-  });
+    jQ('#savedViewHUDButton').after("<div class='showCompletedButton button'><div class='topBarButtonTextContainer'><a href='#' id='recentLink_1wk'>1wk</a></div></div>");
+    jQ('#recentLink_1wk').click(function(e) {
+      e.preventDefault();
+      if (jQ('#searchBox').val()=='last-changed:7d') {
+        search.searchProjectTree('');
+      } else {
+        search.searchProjectTree('last-changed:7d');
+      }
+    });
+    jQ('#savedViewHUDButton').after("<div class='showCompletedButton button'><div class='topBarButtonTextContainer'><a href='#' class='button' id='recentLink_24hrs'>24hrs</a></div></div>");
+    jQ('#recentLink_24hrs').click(function(e) {
+      e.preventDefault();
+      if (jQ('#searchBox').val()=='last-changed:1d') {
+        search.searchProjectTree('');
+      } else {
+        search.searchProjectTree('last-changed:1d');
+      }
+    });
+
+  // Add -rand functionality
+    jQ(window).on('hashchange',function(e) {
+      if (searching) {
+        return false;
+      }
+
+      var query = jQ('#searchBox').val();
+      var needle=/(%23\w*-rand)+/;
+      var match = window.location.href.match(needle);
+      if (match) {
+        // A tag with -rand on the end has been clicked. Locate another.
+        searching = true;
+        var tag = match[0]; //matches "2 chapters"
+        tag = "#"+tag.slice(3);
+        window.location.href='/#';
+        search.searchProjectTree(tag);
+        var target = null;
+        var count = 0;
+        var tags = $('.contentMatch');
+        var random = $(tags[Math.floor(Math.random()*tags.length)])[0];
+        var parent = jQ(random).parents('.name').find('a').first();
+        var href = jQ(parent).attr('href');
+        window.location.href = href;
+        setTimeout(function() {
+          searching = false;
+        },100);
+      }
+    });
 }
 
 // load jQuery and execute the main function
